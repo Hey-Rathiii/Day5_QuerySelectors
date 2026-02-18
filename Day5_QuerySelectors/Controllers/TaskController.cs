2975a38347032007855700a9b49a750fe3b9c481
Ayush Rathi <rathiaccess@gmail.com>
2026-02-18 23:37:25 +0530

    Add project files.

---
+++ Day5_QuerySelectors/Controllers/TaskController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day5_QuerySelectors.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Highlight()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day5_QuerySelectors/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Day5_QuerySelectors.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
